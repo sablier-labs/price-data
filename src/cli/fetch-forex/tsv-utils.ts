91587a73edcc23d76e8ca32cb45aef922e08953d
Paul Razvan Berg <prberg@proton.me>
2025-10-13 18:32:04 +0300

    chore: delete stale import

--- src/cli/fetch-forex/tsv-utils.ts
+++ src/cli/fetch-forex/tsv-utils.ts
@@ -1,6 +1,5 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
-import chalk from "chalk";
 
 /* -------------------------------------------------------------------------- */
 /*                                    TYPES                                   */
